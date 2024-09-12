52b40cd38f2b7fdf4d85cbbdc82ad195bfd06371
Prajwal <im.mht01@gmail.com>
2024-09-12 20:15:43 +0545

    async

--- GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b870cd7095f086b1c9fad17b371e05ab6dc3cbc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e346f7cec0919ecd7169790b7357702a9296289")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
