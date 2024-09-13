ff23b31e562bce946b05109c9749f4cb837171c0
Prajwal <im.mht01@gmail.com>
2024-09-13 12:46:40 +0545

    frontend

--- GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52b40cd38f2b7fdf4d85cbbdc82ad195bfd06371")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfe47ca4ef8a3b180948177d12c53cfd74a5c2f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
